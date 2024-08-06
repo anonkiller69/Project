28d76410aebd16b95897e01c163dc3152a7478aa
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 13:18:32 +0000

    gfgf

--- alteracao-main/app_jogos/view/esqueleto_app.py
+++ alteracao-main/app_jogos/view/esqueleto_app.py
@@ -1,4 +1,30 @@
 from tkinter import ttk
 import tkinter as tk
-from .elementos_tkinter import Labelcustomizada, Buttoncustomizado, CheckButtoncustomizado, LabelcustomizadaTitulo, Mensagens
+from .elementos_tkinter import Labelcustomizada, Buttoncustomizado, CheckButtoncustomizado, LabelcustomizadaTitulo, Mensagens, Framecustomizado
 from controle.controle import Controle
+
+
+class Header:
+    pass
+
+class Body:
+    def __init__(self, root):
+        self.root = root
+        self.container_frame = Framecustomizado(self.root, width=400, height=400, bg='blue')
+        self.container_frame.grid(row=0, column=0)
+        
+    
+class AppGames:
+    def __init__(self, root : tk):
+        self.root = root
+        self.root.title('AppRom')
+    
+    def principal(self):
+        self.body_app = Body(self.root)
+
+root = tk.Tk()
+app = AppGames(root)
+root.mainloop()
+    
+
+        
