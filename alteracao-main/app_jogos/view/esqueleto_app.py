9221cbda9ec73e3d8a82858491cd4ba1b09ea9df
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 13:36:56 +0000

    cvcv

--- alteracao-main/app_jogos/view/esqueleto_app.py
+++ alteracao-main/app_jogos/view/esqueleto_app.py
@@ -7,21 +7,32 @@ from elementos_tkinter import Labelcustomizada, Buttoncustomizado, CheckButtoncu
 class Header:
     pass
 
+        
+        
+
 class Body:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, principal):
+        self.principal = principal
         self.container_frame = Framecustomizado(self.root, width=400, height=400, bg='blue')
         self.container_frame.grid_propagate(False)
         self.container_frame.grid(row=0, column=0)
+        self.card_frame()
+
+    #as imagens e os jogos estarão dentro de um frame
+    def card_frame(self):
+        self.card = Framecustomizado(self.container_frame, bg='red', width=200, height=200)
+        self.card.grid(row=5, column=0)
+
+
         
     
 class AppGames:
     def __init__(self, root):
         self.root = root
         self.root.title('AppRom')
-        self.principal()
+        self.corpo()
     
-    def principal(self):
+    def corpo(self):
         self.body_app = Body(self.root)
 
 root = tk.Tk()
