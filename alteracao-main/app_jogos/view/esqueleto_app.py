89dcdeba50c6b8064474729cae869f06ced04013
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 13:21:14 +0000

    dfd

--- alteracao-main/app_jogos/view/esqueleto_app.py
+++ alteracao-main/app_jogos/view/esqueleto_app.py
@@ -1,7 +1,7 @@
 from tkinter import ttk
 import tkinter as tk
 from elementos_tkinter import Labelcustomizada, Buttoncustomizado, CheckButtoncustomizado, LabelcustomizadaTitulo, Mensagens, Framecustomizado
-from controle.controle import Controle
+
 
 
 class Header:
