ecf9ed723fa3671ec157cce91fee73259f330e76
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 14:14:05 +0000

    sdsds

--- alteracao-main/app_jogos/view/images.py
+++ alteracao-main/app_jogos/view/images.py
@@ -7,7 +7,7 @@ class CarregaImagem:
         self.url_imagem = url_imagem
         self.width_padrao = 200
         self.height_padrao = 200
-        self.base_path = "C:/Users/mateu/Project/alteracao-main/app_jogos/view"
+        self.base_path = "C:/Users/mateu/Project/alteracao-main/app_jogos/view/"
         self.image = Image.open(self.base_path + self.url_imagem).resize((self.width_padrao, self.width_padrao))
         self.photo = ImageTk.PhotoImage(self.image)
         
