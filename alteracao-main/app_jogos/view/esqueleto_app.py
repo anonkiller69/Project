e606acc8345af7374d1a3039f7f26e4bdb4e1266
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 13:05:46 +0000

    fdfdf

--- alteracao-main/app_jogos/view/esqueleto_app.py
+++ alteracao-main/app_jogos/view/esqueleto_app.py
@@ -1,3 +1,4 @@
 from tkinter import ttk
 import tkinter as tk
 from .elementos_tkinter import Labelcustomizada, Buttoncustomizado, CheckButtoncustomizado, LabelcustomizadaTitulo, Mensagens
+from controle.controle import Controle
