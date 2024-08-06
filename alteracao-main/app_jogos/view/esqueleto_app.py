04ae85ac36ff06a1abf239fe7bdc7fb206adc7c2
anonkiller69 <mateusdorvis@gmail.com>
2024-08-06 13:22:11 +0000

    dfd

--- alteracao-main/app_jogos/view/esqueleto_app.py
+++ alteracao-main/app_jogos/view/esqueleto_app.py
@@ -11,6 +11,7 @@ class Body:
     def __init__(self, root):
         self.root = root
         self.container_frame = Framecustomizado(self.root, width=400, height=400, bg='blue')
+        self.container_frame.grid_propagate(False)
         self.container_frame.grid(row=0, column=0)
         
     
